905fd97cd5607447e71ee65baa76466642468cb6
plugin-service <54523289+plugin-service@users.noreply.github.com>
2019-10-08 15:32:39 +0530

    Update ZoomAuthEval.js

--- www/ZoomAuthEval.js
+++ www/ZoomAuthEval.js
@@ -1,25 +1,25 @@
   var exec = require('cordova/exec');
 
   exports.initialize = function(appToken, success, error) {
-    exec(success, error, "ZoomAuthentication", "initialize", [appToken]);
+    exec(success, error, "ZoomAuthEval", "initialize", [appToken]);
   };
   //Above function for initializing the SDK. A valid appToken should be passed
   //Output similar to getSdkStatus function below
 
   exports.enroll = function(userId, success, error) {
-    exec(success, error, "ZoomAuthentication", "enroll", [userId]);
+    exec(success, error, "ZoomAuthEval", "enroll", [userId]);
   }
   //Above function will only work in full version as this require server side integration
   //Above function is for enrolling a new user. Unique userId should be passed
 
   exports.authenticate = function(userId, success, error) {
-    exec(success, error, "ZoomAuthentication", "authenticate", [userId]);
+    exec(success, error, "ZoomAuthEval", "authenticate", [userId]);
   }
   //Above function will only work in full version as this require server side integration
   //Above function is for authenticating an already enrolled user. userId of the user should be passed
 
   exports.verify = function(success, error) {
-    exec(success, error, "ZoomAuthentication", "verify", []);
+    exec(success, error, "ZoomAuthEval", "verify", []);
   }
   //Above function is for doing liveness check on the device and the server
   //Output could be one of the following
@@ -33,12 +33,12 @@
     //"Source data length is < 20",
 
   exports.getVersion = function(success, error) {
-    exec(success, error, "ZoomAuthentication", "getVersion", []);
+    exec(success, error, "ZoomAuthEval", "getVersion", []);
   };
   //Above function is for checking the SDK version
 
   exports.getSdkStatus = function(success, error) {
-    exec(success, error, "ZoomAuthentication", "getSdkStatus", []);
+    exec(success, error, "ZoomAuthEval", "getSdkStatus", []);
   }
   //Above function is for checking the status of the SDK
   //Output could be one of the below
@@ -56,7 +56,7 @@
     //"UnknownStatus";
 
   exports.getUserEnrollmentStatus = function(userId, success, error) {
-    exec(success, error, "ZoomAuthentication", "getUserEnrollmentStatus", [userId]);
+    exec(success, error, "ZoomAuthEval", "getUserEnrollmentStatus", [userId]);
   }
   //Above function will only work in full version as this require server side integration
   //Above function is for checking the enrollment status of an user. userId of the user should be passed.
@@ -68,7 +68,7 @@
         success(status == "Enrolled");
       }
     }
-    exec(onSuccess, error, "ZoomAuthentication", "getUserEnrollmentStatus", [userId]);
+    exec(onSuccess, error, "ZoomAuthEval", "getUserEnrollmentStatus", [userId]);
   }
   //Above function is for checking if a user is already enrolled or not. userId of the user should be passed.
   //Output is true or false
@@ -82,21 +82,49 @@
   //Please check with FaceTec support team to enable production mode as well as to register the public key.
 
   exports.setServerUrl = function(serverUrl, success, error) {
-    exec(success, error, "ZoomAuthentication", "setServerUrl", [serverUrl]);
+    exec(success, error, "ZoomAuthEval", "setServerUrl", [serverUrl]);
   }
   //Above function is for setting the zoom server url if it is different than the default one
 
   exports.setBrandingLogo = function(brandingLogo, success, error) {
-    exec(success, error, "ZoomAuthentication", "setBrandingLogo", [brandingLogo]);
+    exec(success, error, "ZoomAuthEval", "setBrandingLogo", [brandingLogo]);
   }
   //Above function is for setting custom logo in the zoom screens
 
-  exports.setMainForeGroundColors = function(foreGroundColors, success, error) {
-    exec(success, error, "ZoomAuthentication", "setForeGroundColors", [foreGroundColors]);
+  exports.setMainForeGroundColor = function(foreGroundColor, success, error) {
+    exec(success, error, "ZoomAuthEval", "setMainForeGroundColor", [foreGroundColor]);
   }
   //Above function is for setting the custom fore ground color of zoom main screens
 
-  exports.setMainBackGroundColors = function(backGroundColors, success, error) {
-    exec(success, error, "ZoomAuthentication", "setBackGroundColors", [backGroundColors]);
+  exports.setMainBackGroundColor = function(backGroundColor, success, error) {
+    exec(success, error, "ZoomAuthEval", "setMainBackGroundColor", [backGroundColor]);
   }
   //Above function is for setting the custom back ground color of zoom main screens
+
+  exports.idCheck = function( success, error) {
+    exec(success, error, "ZoomAuthEval", "idCheck", []);
+  }
+
+  exports.deleteEnrollment = function(userId, success, error) {
+    exec(success, error, "ZoomAuthEval", "deleteEnrollment", [userId]);
+  }
+
+  exports.auditTrail = function( success, error) {
+    exec(success, error, "ZoomAuthEval", "auditTrail", []);
+  }
+
+  exports.setDefaultTheme = function( success, error) {
+    exec(success, error, "ZoomAuthEval", "setDefaultTheme", []);
+  }
+
+  exports.setDarkTheme = function( success, error) {
+    exec(success, error, "ZoomAuthEval", "setDarkTheme", []);
+  }
+
+  exports.setMidnightTheme = function( success, error) {
+    exec(success, error, "ZoomAuthEval", "setMidnightTheme", []);
+  }
+
+  exports.setSunsetTheme = function( success, error) {
+    exec(success, error, "ZoomAuthEval", "setSunsetTheme", []);
+  }
